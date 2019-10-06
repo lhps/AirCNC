096338c27fabf5067f43c33632f131240e05114b
Lucas <lucashpsfei@gmail.com>
2019-10-06 18:29:29 -0300

    FrontEnd finalizado

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+
 
 const routes = require('./routes');
 
@@ -15,8 +18,9 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-b89wo.mongodb.net/
 // req.query = Acessar query parameters (para filtros)
 // req.params = Acessar route parameters (para edição - put e delete)
 // req.body = Acessar corpo da requisição ( para criação e edição)
-
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname,'..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
